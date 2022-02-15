93078df2f52e9afc309748d52e69dde77393241f
imollov <ivomollov@gmail.com>
2022-02-15 19:38:36 +0200

    fix: convert address to string correctly

--- contracts/UTT.sol
+++ contracts/UTT.sol
@@ -200,9 +200,7 @@ contract UTT is ERC20Burnable, ERC20Pausable, Ownable, ChainlinkClient {
     function endorse(address target, uint256 amount) external {
         require(msg.sender == tx.origin, "should be an user");
         Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfillEndorse.selector);
-        request.add("targetAddress", string(abi.encodePacked(target)));
-        sendOperatorRequestTo(oracle, request, fee);
-        oracleRequests[asciiToInteger(request.id)] = OracleRequest({ from: msg.sender, target: target, amount: amount });
+        request.add("targetAddress", toAsciiString(target));
     }
 
     function fulfillEndorse(
@@ -292,6 +290,23 @@ contract UTT is ERC20Burnable, ERC20Pausable, Ownable, ChainlinkClient {
                 break;
         }
         return y;
+    }
+
+    function toAsciiString(address x) internal pure returns (string memory) {
+        bytes memory s = new bytes(40);
+        for (uint i = 0; i < 20; i++) {
+            bytes1 b = bytes1(uint8(uint(uint160(x)) / (2**(8*(19 - i)))));
+            bytes1 hi = bytes1(uint8(b) / 16);
+            bytes1 lo = bytes1(uint8(b) - 16 * uint8(hi));
+            s[2*i] = char(hi);
+            s[2*i+1] = char(lo);
+        }
+        return string(s);
+    }
+
+    function char(bytes1 b) internal pure returns (bytes1 c) {
+        if (uint8(b) < 10) return bytes1(uint8(b) + 0x30);
+        else return bytes1(uint8(b) + 0x57);
     }
 
     function stringToBytes32(string memory source) public pure returns (bytes32 result) {
