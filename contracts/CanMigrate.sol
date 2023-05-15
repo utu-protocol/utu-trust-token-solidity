b8c5bce071dafa3b46bc3cf062675c5d63bb6cba
musayann <musayann@gmail.com>
2023-05-15 17:23:20 +0200

    chore: remove invalid imports

--- contracts/CanMigrate.sol
+++ contracts/CanMigrate.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.7;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "./SocialConnector.sol";
 
 abstract contract CanMigrate is ERC20, Ownable {
     /** Contract migration data flag; when migrating data from the old contract. */
