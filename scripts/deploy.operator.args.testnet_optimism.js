a424d131a7dd254a8053032190aaf72d8b802674
Bastian <bastian@maramoja.co.ke>
2023-10-18 20:39:12 +0300

    Add configs for Optimism (testnet)

---
+++ scripts/deploy.operator.args.testnet_optimism.js
@@ -0,0 +1,7 @@
+const { ethers } = require("hardhat");
+
+module.exports = [
+  // optimism goerli testnet link token address according to
+  // https://docs.chain.link/resources/link-token-contracts#optimism-goerli-testnet
+  "0xdc2CC710e42857672E7907CF474a69B63B93089f",
+];
