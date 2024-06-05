7da51eac56e188ea770b3e3d968e869e896636dc
Jebitok-dev <sharonkosgei4@gmail.com>
2024-06-05 15:14:36 +0300

    Add Changes to the Testnet Ethereum Link Address

--- scripts/deploy.operator.args.testnet_ethereum.js
+++ scripts/deploy.operator.args.testnet_ethereum.js
@@ -1,5 +1,5 @@
 const { ethers } = require("hardhat");
 
 module.exports = [
-  "0x0Fd9e8d3aF1aaee056EB9e802c3A762a667b1904", // Ethereum testnet link token address
+  "0x779877A7B0D9E8603169DdbD7836e478b4624789", // Ethereum testnet link token address
 ];
