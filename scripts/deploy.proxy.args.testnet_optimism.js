e70541e3cce841dd3c7fb64db00ff28248e1190c
Bastian <bastian@maramoja.co.ke>
2024-10-21 17:58:02 +0300

    Fix proxy deployment arg files.

--- scripts/deploy.proxy.args.testnet_optimism.js
+++ scripts/deploy.proxy.args.testnet_optimism.js
@@ -5,7 +5,7 @@ const { ethers } = require("hardhat");
 module.exports = [
   "0x6934c1F62a6d28a573E2b4071a754DDd29B81E54", //  oracle operator contract
   "18cb4a46518e48c9b753446386b16bac", // job id for UTT Proxy Endorse
-  ethers.("0.0001"), // operator LINK fee
+  ethers.parseEther("0.0000001"), // operator LINK fee
   "0xdc2CC710e42857672E7907CF474a69B63B93089f", // link token address
   "cdf5d5acb468435ea51ad2798e80f15e", // job id for UTT Proxy Claim Rewards
 ];
--- scripts/deploy.proxy.args.testnet_polygon.js
+++ scripts/deploy.proxy.args.testnet_polygon.js
@@ -4,6 +4,6 @@ const { ethers } = require("hardhat");
 module.exports = [
   "0xf64991a3C1C448df967e5DC8e8Cc1D3b3BD0034f", // testnet_polygon oracle
   "0eec7e1dd0d2476ca1a872dfb6633f48", // testnet_polygon job id
-  ethers.("0.0001"), // testnet_polygon fee
+  ethers.parseEther("0.0000001"), // testnet_polygon fee
   "0x326C977E6efc84E512bB9C30f76E30c160eD06FB", // testnet_polygon link token address
 ];
