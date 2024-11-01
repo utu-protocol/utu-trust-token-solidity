1418c3f96ea092a50747391be05e2a01f5e3ed43
Bastian <bastian@maramoja.co.ke>
2024-11-01 12:36:27 +0300

    Update new optimism testnet (Sepolia) argument files.

--- scripts/deploy.operator.args.testnet_optimism.js
+++ scripts/deploy.operator.args.testnet_optimism.js
@@ -1,7 +1,7 @@
 const { ethers } = require("hardhat");
 
 module.exports = [
-  // Optimism Ssepolia testnet link token address according to
+  // Optimism Sepolia testnet link token address according to
   // https://docs.chain.link/resources/link-token-contracts#optimism-goerli-testnet
   "0xE4aB69C077896252FAFBD49EFD26B5D171A32410",
 ];
--- scripts/deploy.proxy.args.testnet_optimism.js
+++ scripts/deploy.proxy.args.testnet_optimism.js
@@ -6,6 +6,6 @@ module.exports = [
   "0x6934c1F62a6d28a573E2b4071a754DDd29B81E54", //  oracle operator contract
   "18cb4a46518e48c9b753446386b16bac", // job id for UTT Proxy Endorse
   ethers.parseEther("0.0000001"), // operator LINK fee
-  "0xdc2CC710e42857672E7907CF474a69B63B93089f", // link token address
+  "0xE4aB69C077896252FAFBD49EFD26B5D171A32410", // link token address
   "cdf5d5acb468435ea51ad2798e80f15e", // job id for UTT Proxy Claim Rewards
 ];
