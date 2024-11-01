cde132d712c68ac09d3dd2f0fc7d0c24b0043ce2
Bastian <bastian@maramoja.co.ke>
2024-11-01 16:24:14 +0300

    48: remove obsolete goerli-related comment in testnet parameter files.

--- scripts/deploy.operator.args.testnet_optimism.js
+++ scripts/deploy.operator.args.testnet_optimism.js
@@ -2,6 +2,6 @@ const { ethers } = require("hardhat");
 
 module.exports = [
   // Optimism Sepolia testnet link token address according to
-  // https://docs.chain.link/resources/link-token-contracts#optimism-goerli-testnet
+  // https://docs.chain.link/resources/link-token-contracts#optimism-sepolia-testnet
   "0xE4aB69C077896252FAFBD49EFD26B5D171A32410",
 ];
