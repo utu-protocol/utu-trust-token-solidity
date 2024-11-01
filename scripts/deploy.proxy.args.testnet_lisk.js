bb20726ae33da530d183c7e8660fb5408d9d1ecc
Bastian <bastian@maramoja.co.ke>
2024-11-01 16:24:45 +0300

    48: fix job id in LISK testnet parameter file.

--- scripts/deploy.proxy.args.testnet_lisk.js
+++ scripts/deploy.proxy.args.testnet_lisk.js
@@ -4,8 +4,8 @@ const { ethers } = require("hardhat");
 // value for proxy on optimism goerli testnet
 module.exports = [
   "0xC3586558ddb1Cc6C7c5338691842b8d5F47D253d", //  oracle operator contract
-  "18cb4a46518e48c9b753446386b16bac", // job id for UTT Proxy Endorse
+  "30d3f168244f40788be35c05f6c5924f", // job id for UTT Proxy Endorse
   ethers.parseEther("0.0000001"), // operator LINK fee
   "0xE4aB69C077896252FAFBD49EFD26B5D171A32410", // link token address
-  "cdf5d5acb468435ea51ad2798e80f15e", // job id for UTT Proxy Claim Rewards
+  "2d3086720bcd4847bf088ffb976d9508", // job id for UTT Proxy Claim Rewards
 ];
