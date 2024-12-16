1da0f481569faeb182004a6b80eb861300c55bce
Bastian <bastian@maramoja.co.ke>
2024-12-16 10:06:38 +0100

    #48: fix LINK token address in LISK main- and testnet deploy args.

--- scripts/deploy.proxy.args.lisk.js
+++ scripts/deploy.proxy.args.lisk.js
@@ -6,6 +6,6 @@ module.exports = [
   "0x68d806F671dcBdaF0bB7f4E836EE2dFe30Ba131C", //  oracle operator contract
   "4c489da76e08405b8cf521afc6a65cd5", // job id for UTT Proxy Endorse
   ethers.parseEther("0.0000001"), // operator LINK fee
-  "0xE4aB69C077896252FAFBD49EFD26B5D171A32410", // link token address
+  "0xB7dA7E0B8C1f82A647066C34eE2317A5AC6Adc09", // link token address
   "dd1d470112d74e1f847c499b57ca5495", // job id for UTT Proxy Claim Rewards
 ];
--- scripts/deploy.proxy.args.testnet_lisk.js
+++ scripts/deploy.proxy.args.testnet_lisk.js
@@ -6,6 +6,6 @@ module.exports = [
   "0xC3586558ddb1Cc6C7c5338691842b8d5F47D253d", //  oracle operator contract
   "30d3f168244f40788be35c05f6c5924f", // job id for UTT Proxy Endorse
   ethers.parseEther("0.0000001"), // operator LINK fee
-  "0xE4aB69C077896252FAFBD49EFD26B5D171A32410", // link token address
+  "0x6934c1F62a6d28a573E2b4071a754DDd29B81E54", // link token address
   "2d3086720bcd4847bf088ffb976d9508", // job id for UTT Proxy Claim Rewards
 ];
