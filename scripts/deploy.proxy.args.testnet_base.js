0c89cf5eb0ead8cdb366f08b4c37caef5fe204b3
Bastian <bastian@maramoja.co.ke>
2025-09-17 18:08:36 +0300

    Update Base testnet proxy contract delpoyment args after re-deployment of oracle operator, and fix job ids.

--- scripts/deploy.proxy.args.testnet_base.js
+++ scripts/deploy.proxy.args.testnet_base.js
@@ -3,9 +3,9 @@ const { ethers } = require("hardhat");
 
 // value for proxy on base sepolia testnet
 module.exports = [
-  "0xC3586558ddb1Cc6C7c5338691842b8d5F47D253d", //  oracle operator contract
-  "30d3f168244f40788be35c05f6c5924f", // job id for UTT Proxy Endorse
+  "0x1380FD912C44F3860D17EB6221F861F9c4611D97", //  oracle operator contract
+  "89c86d27881e4854a61a87070363d7d7", // job id for UTT Proxy Endorse
   ethers.parseEther("0.0000001"), // operator LINK fee
-  "0x778D2e75d1D09aDd600f2f1C44c9980C4c63f24F", // link token address
-  "2d3086720bcd4847bf088ffb976d9508", // job id for UTT Proxy Claim Rewards
+  "0xE4aB69C077896252FAFBD49EFD26B5D171A32410", // link token address
+  "6cdee6e7c62b43469926f7dc7bc8ffae", // job id for UTT Proxy Claim Rewards
 ];
